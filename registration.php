2b590d7139ec8239dec97926b07234b36a96bc1e
agaday95 <phamminhduc1008@gmail.com>
2021-04-16 15:49:14 +0700

    fix

--- registration.php
+++ registration.php
@@ -6,4 +6,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Magento/raww', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/AHT/raww', __DIR__);
